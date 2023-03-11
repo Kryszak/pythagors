7aea97ddca0bad852eba88da4ea1b3be9fb81a12
Kryszak <prajskrzysztof@gmail.com>
2023-03-11 12:58:33 +0100

    migrate project to shuttle structure

--- src/globals.rs
+++ src/globals.rs
@@ -1,6 +1,5 @@
-use std::env;
-
 use serde_json::Value;
+use shuttle_secrets::SecretStore;
 
 #[derive(Clone)]
 pub struct Globals {
@@ -14,23 +13,30 @@ pub struct Globals {
 }
 
 impl Globals {
-    pub fn new() -> Self {
-        dotenv::dotenv().ok();
+    pub fn new(secret_store: SecretStore) -> Self {
         Globals {
-            client_token: env::var("CLIENT_TOKEN").expect("No client token provided"),
-            watched_channel: env::var("WATCHED_CHANNEL")
+            client_token: secret_store
+                .get("CLIENT_TOKEN")
+                .expect("No client token provided"),
+            watched_channel: secret_store
+                .get("WATCHED_CHANNEL")
                 .expect("No channel name provided to watch"),
-            message_fetch_limit: env::var("MESSAGE_FETCH_LIMIT")
-                .unwrap_or_else(|_| String::from("50"))
+            message_fetch_limit: secret_store
+                .get("MESSAGE_FETCH_LIMIT")
+                .unwrap_or_else(|| String::from("50"))
                 .parse::<i32>()
                 .expect("Failed to parse message limit count"),
-            wrong_number_message_template: env::var("WRONG_INCREMENT_MESSAGE_TEMPLATE")
+            wrong_number_message_template: secret_store
+                .get("WRONG_INCREMENT_MESSAGE_TEMPLATE")
                 .expect("No message template for wrong number posted provided"),
-            wrong_format_message_template: env::var("WRONG_FORMAT_MESSAGE_TEMPLATE")
+            wrong_format_message_template: secret_store
+                .get("WRONG_FORMAT_MESSAGE_TEMPLATE")
                 .expect("No message template for wrong format posted provided"),
-            rank_won_message_template: env::var("RANK_WON_MESSAGE_TEMPLATE")
+            rank_won_message_template: secret_store
+                .get("RANK_WON_MESSAGE_TEMPLATE")
                 .expect("No message template for rank won provided"),
-            ranks: env::var("RANKS")
+            ranks: secret_store
+                .get("RANKS")
                 .map(|data| serde_json::from_str(&data).expect("failed to parse data"))
                 .expect("No prized numbers provided"),
         }
---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+mod discord;
+mod globals;
+mod verification;
+
+use anyhow::anyhow;
+use globals::Globals;
+use serenity::{prelude::GatewayIntents, Client};
+use shuttle_secrets::SecretStore;
+use verification::MessageVerificator;
+
+#[shuttle_service::main]
+async fn serenity(
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> shuttle_service::ShuttleSerenity {
+    let token = if let Some(token) = secret_store.get("CLIENT_TOKEN") {
+        token
+    } else {
+        return Err(anyhow!("'CLIENT_TOKEN' was not found").into());
+    };
+
+    let globals = Globals::new(secret_store);
+    let message_verificator = MessageVerificator::new(globals);
+    let intents =
+        GatewayIntents::GUILDS | GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let client = Client::builder(&token, intents)
+        .event_handler(message_verificator)
+        .await
+        .expect("Err creating client");
+
+    Ok(client)
+}
