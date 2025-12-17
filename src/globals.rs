01b4af888c9373f8ab987184e32645d9295c67d7
Kryszak <prajskrzysztof@gmail.com>
2025-12-17 22:38:48 +0100

    removed shuttle dependencies

--- src/globals.rs
+++ src/globals.rs
@@ -1,8 +1,8 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fs};
 
-use shuttle_runtime::SecretStore;
+use toml::Value;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Globals {
     pub client_token: String,
     pub watched_channel: String,
@@ -16,37 +16,56 @@ pub struct Globals {
 }
 
 impl Globals {
-    pub fn new(secret_store: SecretStore) -> Self {
+    pub fn new() -> Self {
+        let secret_file = fs::read_to_string("Secrets.toml").unwrap();
+        let map: HashMap<String, Value> = toml::from_str(&secret_file).unwrap();
         Globals {
-            client_token: secret_store
+            client_token: map
                 .get("CLIENT_TOKEN")
+                .map(|x| x.as_str().unwrap())
+                .map(String::from)
                 .expect("No client token provided"),
-            watched_channel: secret_store
+            watched_channel: map
                 .get("WATCHED_CHANNEL")
+                .map(|x| x.as_str().unwrap())
+                .map(String::from)
                 .expect("No channel name provided to watch"),
-            message_fetch_limit: secret_store
+            message_fetch_limit: map
                 .get("MESSAGE_FETCH_LIMIT")
+                .map(|x| x.as_str().unwrap())
+                .map(String::from)
                 .unwrap_or_else(|| String::from("50"))
                 .parse::<i32>()
                 .expect("Failed to parse message limit count"),
-            wrong_number_message_template: secret_store
+            wrong_number_message_template: map
                 .get("WRONG_INCREMENT_MESSAGE_TEMPLATE")
+                .map(|x| x.as_str().unwrap())
+                .map(String::from)
                 .expect("No message template for wrong number posted provided"),
-            wrong_format_message_template: secret_store
+            wrong_format_message_template: map
                 .get("WRONG_FORMAT_MESSAGE_TEMPLATE")
+                .map(|x| x.as_str().unwrap())
+                .map(String::from)
                 .expect("No message template for wrong format posted provided"),
-            rank_won_message_template: secret_store
+            rank_won_message_template: map
                 .get("RANK_WON_MESSAGE_TEMPLATE")
+                .map(|x| x.as_str().unwrap())
+                .map(String::from)
                 .expect("No message template for rank won provided"),
-            game_over_message_template: secret_store
+            game_over_message_template: map
                 .get("GAME_OVER_MESSAGE_TEMPLATE")
+                .map(|x| x.as_str().unwrap())
+                .map(String::from)
                 .expect("No message template for game over provided"),
-            ranks: secret_store
+            ranks: map
                 .get("RANKS")
-                .map(|data| serde_json::from_str(&data).expect("failed to parse ranks json"))
+                .map(|x| x.as_str().unwrap())
+                .map(|data| serde_json::from_str(data).expect("failed to parse ranks json"))
                 .expect("No prized numbers provided"),
-            gameover_number: secret_store
+            gameover_number: map
                 .get("GAME_OVER_NUMBER")
+                .map(|x| x.as_str().unwrap())
+                .map(String::from)
                 .expect("No game over number provided")
                 .parse::<i32>()
                 .expect("Failed to parse game over number"),
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,20 @@ mod verification;
 
 use globals::Globals;
 use serenity::prelude::*;
-use shuttle_runtime::SecretStore;
 use std::sync::Arc;
+use tokio::signal;
+use tracing::error;
 use verification::MessageVerificator;
 
-#[shuttle_runtime::main]
-async fn serenity(
-    #[shuttle_runtime::Secrets] secret_store: SecretStore,
-) -> shuttle_serenity::ShuttleSerenity {
+async fn shutdown_signal() {
+    // Ctrl+C
+    signal::ctrl_c()
+        .await
+        .expect("Failed to install Ctrl+C handler");
+}
+
+#[tokio::main]
+async fn main() {
     tracing_subscriber::fmt()
         .with_ansi(true)
         .with_file(false)
@@ -20,16 +26,28 @@ async fn serenity(
         .without_time()
         .init();
 
-    let globals = Arc::new(Globals::new(secret_store));
+    let globals = Arc::new(Globals::new());
+    println!("{:?}", globals);
     let token = globals.client_token.clone();
     let message_verificator = MessageVerificator::new(globals);
     let intents =
         GatewayIntents::GUILDS | GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
-    let client = Client::builder(&token, intents)
+    let mut client = Client::builder(&token, intents)
         .event_handler(message_verificator)
         .await
         .expect("Err creating client");
 
-    Ok(client.into())
+    // Spawn shutdown listener
+    let shard_manager = client.shard_manager.clone();
+    tokio::spawn(async move {
+        shutdown_signal().await;
+        println!("Shutdown signal received. Shutting down...");
+        shard_manager.shutdown_all().await
+    });
+
+    // Start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        error!("Client error: {why:?}");
+    }
 }
